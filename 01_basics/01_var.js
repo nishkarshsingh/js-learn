8bd0d8e733536148931d12ef8b45179b95b4797d
nishkarsh singh <40988489+nishkarshsingh@users.noreply.github.com>
2025-07-11 06:58:36 +0000

    datatypes

---
+++ 01_basics/01_var.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail ="nishkarsh@gmail.com"
+var accountPswd = "12345"
+accountCity = "Delhi"
+let accountstate;
+
+// accountId = 2 
+
+accountEmail = "hcl@gmail.com"
+accountPswd = "212121"
+accountCity = "jaipur"
+
+console.log(accountId);
+/*
+prefer not to use var
+*/
+console.table([accountEmail, accountId, accountPswd, accountCity,accountstate]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as new version
+
+// alert( 3 + 3) // we are suing nodejs, not browser
+
+let name ="nishkarsh"
+let age = 21
+let isLoggedIn = false
+let state       // undefined 
+let state1 = null // empty
+
+// number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof age); 
+console.log(typeof null); //object
